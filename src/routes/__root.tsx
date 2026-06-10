8f9186e31d2f73c4770d092c919da661b3c90dca
farzanehmalekyari <farzanehmalekyari@gmail.com>
2026-06-11 00:14:44 +0330

    Font changed

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -77,7 +77,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;500;600;700;800;900&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Parastoo:wght@400;700&display=swap",
       },
     ],
   }),
